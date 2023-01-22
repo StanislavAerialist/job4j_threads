6610ae1018c29db34fca0fd446c01d416ff23f29
StanislavAerialist <moonlight_stas@mail.ru>
2023-01-22 19:32:37 +0500

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        System.out.println("Start loading ... ");
+                        Thread.sleep(1000);
+                        for (int i = 0; i < 101; i++) {
+                            System.out.print("\rLoading : " + i  + "%");
+                            Thread.sleep(200);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+        System.out.println("Main");
+    }
+}
