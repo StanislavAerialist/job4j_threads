d3755ed3fdf55b77520e917626a24cd30e8ecdcb
StanislavAerialist <moonlight_stas@mail.ru>
2023-01-23 22:08:48 +0500

    8. Скачивание файла с ограничением. [#144271 #383882]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -5,6 +5,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
+import static java.lang.System.currentTimeMillis;
+
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
@@ -16,11 +18,23 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
+        String[] name = url.split("/");
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("downloaded file")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(name[name.length - 1])) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            long start = currentTimeMillis();
+            int downloadData = 0;
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                downloadData += bytesRead;
+                if (downloadData >= speed) {
+                    long procedure = currentTimeMillis() - start;
+                    if (procedure < 1000) {
+                        Thread.sleep(1000 - procedure);
+                    }
+                    downloadData = 0;
+                    start = currentTimeMillis();
+                }
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
                 Thread.sleep(1000);
             }
