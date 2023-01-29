7858c7fd597aafc42b1dc66b3c486972c0d45674
StanislavAerialist <moonlight_stas@mail.ru>
2023-01-29 11:07:23 +0500

    3. Денежные переводы AccountStorage [#1104 #385816]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -11,47 +11,28 @@ public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     public synchronized boolean add(Account account) {
-        boolean rsl = false;
-        if (!accounts.containsKey(account.id())) {
-            accounts.put(account.id(), account);
-            rsl = true;
-        }
-        return rsl;
+        return accounts.putIfAbsent(account.id(), account) == null;
     }
 
     public synchronized boolean update(Account account) {
-        boolean rsl = false;
-        if (accounts.containsKey(account.id())) {
-            accounts.put(account.id(), account);
-            rsl = true;
-        }
-        return rsl;
+        return accounts.replace(account.id(), accounts.get(account.id()), account);
     }
 
     public synchronized boolean delete(int id) {
-        boolean rsl = false;
-        if (accounts.containsKey(id)) {
-            accounts.remove(id);
-            rsl = true;
-        }
-        return rsl;
+        return accounts.remove(id, accounts.get(id));
     }
 
     public synchronized Optional<Account> getById(int id) {
-        Optional<Account> rsl = Optional.empty();
-        if (accounts.containsKey(id)) {
-            rsl = Optional.of(accounts.get(id));
-        }
-        return rsl;
+        return Optional.ofNullable(accounts.get(id));
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
         boolean rsl = false;
-        if (accounts.containsKey(fromId)
-                && accounts.containsKey(toId)
-                && (accounts.get(fromId).amount() - amount >= 0)) {
-            accounts.put(fromId, new Account(fromId, accounts.get(fromId).amount() - amount));
-            accounts.put(toId, new Account(toId, accounts.get(toId).amount() + amount));
+        Account from = accounts.get(fromId);
+        Account to = accounts.get(toId);
+        if (from != null && to != null && (from.amount() - amount >= 0)) {
+            accounts.put(fromId, new Account(fromId, from.amount() - amount));
+            accounts.put(toId, new Account(toId, to.amount() + amount));
             rsl = true;
         }
         return rsl;
