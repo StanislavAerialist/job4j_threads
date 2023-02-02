47ed647a951e092877cd4e46454b78e8455a5562
StanislavAerialist <moonlight_stas@mail.ru>
2023-02-02 21:45:48 +0500

    1. Неблокирующий кеш [#4741 #387315]

--- src/main/java/ru/job4j/cache/Cache.java
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -12,10 +12,6 @@ public class Cache {
 
     public boolean update(Base model) {
         return memory.computeIfPresent(model.getId(), (k, v) -> {
-            Base stored = memory.get(v.getId());
-            if (stored.getVersion() != v.getVersion()) {
-                throw new OptimisticException("Versions are not equal");
-            }
             Base updateBase = new Base(v.getId(), v.getVersion() + 1);
             updateBase.setName(model.getName());
             return updateBase;
