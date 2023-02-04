a19e82d568a5c5425e01a0fbe2cf1b5a4c0d3e9e
StanislavAerialist <moonlight_stas@mail.ru>
2023-02-04 17:42:56 +0500

    1. Неблокирующий кеш [#4741 #387315]

--- src/main/java/ru/job4j/cache/Cache.java
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -5,7 +5,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Cache {
     private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
-
     public boolean add(Base model) {
         return memory.putIfAbsent(model.getId(), model) == null;
     }
