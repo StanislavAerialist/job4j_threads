3f377d3dbe3b2ca4a759f12764b59e680c3e2965
StanislavAerialist <moonlight_stas@mail.ru>
2023-02-09 18:52:29 +0500

    4. CompletableFuture [#361626]

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -1,53 +1,9 @@
 package ru.job4j.pools;
 
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class RolColSum {
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public void setRowSum(int rowSum) {
-            this.rowSum = rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-
-        public void setColSum(int colSum) {
-            this.colSum = colSum;
-        }
-
-        @Override
-        public String toString() {
-            return "Sums{" + "rowSum=" + rowSum
-                    + ", colSum=" + colSum + '}';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Sums sums = (Sums) o;
-            return rowSum == sums.rowSum && colSum == sums.colSum;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(rowSum, colSum);
-        }
-    }
 
     public static Sums[] sum(int[][] matrix) {
         Sums[] rsl = new Sums[matrix.length];
@@ -78,17 +34,6 @@ public class RolColSum {
     }
 
     private static CompletableFuture<Sums> findElementAsync(int[][] matrix, int index) {
-        return CompletableFuture.supplyAsync(() -> {
-            Sums rsl = new Sums();
-            int rowSum = 0;
-            int colSum = 0;
-            for (int i = 0; i < matrix.length; i++) {
-                rowSum = rowSum + matrix[index][i];
-                colSum = colSum + matrix[i][index];
-            }
-            rsl.setRowSum(rowSum);
-            rsl.setColSum(colSum);
-            return rsl;
-        });
+        return CompletableFuture.supplyAsync(() -> findElement(matrix, index));
     }
 }
